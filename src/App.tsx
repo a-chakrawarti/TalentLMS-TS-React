e92e827c9c2929d2e92f68b45f34bcf0176b3698
CURVX <chakrawarti.anupam@gmail.com>
2021-05-06 19:53:46 +0530

    Bootstrapped with CRA, installed TypeScript & Type Definitions

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function sum(a:number, b:number) :number {
+    return a + b
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          {sum(4, 3)}
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
